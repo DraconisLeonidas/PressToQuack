3f7200e66e89d14e4f364e8b3691e34052c2a0c6
DraconisLeonidas <lsr444444@gmail.com>
2022-09-06 14:56:40 -0400

    troubleshooting ver.

---
+++ ModEntry.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using StardewModdingAPI.Utilities;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+
+//I just wanna make the farmer quack on command, because it's funny.
+
+namespace PressToQuack
+{
+    public class ModEntry : Mod
+    {
+        // Add config
+        private ModConfig config;
+                
+        public override void Entry(IModHelper helper)
+        {
+            // Read in config file and create if needed
+            this.config = this.Helper.ReadConfig<ModConfig>();
+
+            // Set up GMCM config when game is launched
+            helper.Events.GameLoop.GameLaunched += SetUpConfig;
+
+            // Set up button pressing doing thing
+            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+
+            /* Old Config stuff
+            this.config = this.Helper.ReadConfig<ModConfig>(); */
+        }
+
+        private void OnGameLaunched(object sender, GameLaunchedEventArgs e)
+        {
+            // get Generic Mod Config Menu's AUP (if installed)
+            var configMenu = this.Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+            if (configMenu is null)
+                return;
+
+            // register mod
+            configMenu.Register(
+                mod: this.ModManifest,
+                reset: () => this.Config = new ModConfig(),
+                save: () => this.Helper.WriteConfig(this.Config)
+                );
+
+            // add config option
+            configMenu.AddKeybindList(
+                mod: this.ModManifest,
+                name: () => "Quack Button",
+                tooltip: () => "The button that makes you quack.",
+                getValue: () => this.Config.QuackButton,
+                setValue: value => this.config.ExampleCheckbox = value
+                );
+        }
+
+        //When button is pressed while player is in game and not in a menu, Quack.
+        private void OnButtonPressed(object sender, ButtonPressedEventArgs e)
+        {
+            //Only allow Quack if player is not in menu
+            if (Context.IsPlayerFree)
+            {
+                //Press button
+                if (e.Button == this.config.QuackButton)
+                {
+                    //Quack (Localized on Player, so audible in Multiplayer! I'm so sorry)
+                    Game1.currentLocation.playSound("Duck");
+
+                }
+
+            }
+        }
+
+    }
+}
